823395b2e9bda907ab47561c97b347307fd45e2e
abehidek <hidek.abe@outlook.com>
2022-05-05 14:52:49 -0300

    Refactor "Herança" (python) class  exercise

--- fundamentos-programação/python/poo/desafio/Personagem/Personagem.py
+++ fundamentos-programação/python/poo/desafio/Personagem/Personagem.py
@@ -1,26 +1,29 @@
 class Personagem:
     def __init__(self, nome, classe, raca):
-        print(f'Construindo em {self} ...')
+        print(f"Construindo em {self} ...")
         self.__nome = nome
         self.__classe = classe
         self.__raca = raca
-    
+
     # Getters
     def get_nome(self):
         return self.__nome
+
     def get_classe(self):
         return self.__classe
+
     def get_raca(self):
         return self.__raca
-    
+
     # Setters
     def set_nome(self, new):
         print("SETTER NOME")
         self.__nome = new
+
     def set_classe(self, new):
         print("SETTER CLASSE")
         self.__classe = new
+
     def set_raca(self, new):
         print("SETTER RAÇA")
         self.__raca = new
-    
--- fundamentos-programação/python/poo/desafio/main.py
+++ fundamentos-programação/python/poo/desafio/main.py
@@ -3,26 +3,24 @@ from Personagem import Classe
 from Personagem import Raça
 
 # from Item import Arma
-import Item.Item as Item
-import Item.Arma as Arma
+# import Item.Item as Item
+# import Item.Arma as Arma
 
-# Classe = Classe.Classe()    # ['Assassino', 'Arqueiro', 'Tanque']
-# Raça = Raça.Raça()          # ['Anão', 'Humano', 'Goblin']
-
-# Jubileu = Personagem.Personagem('Jubileu', Classe.get_classe()[0], Raça.get_raca()[0])
-
-# print(Jubileu.get_nome())
-# print(Jubileu.get_classe())
-# print(Jubileu.get_raca())
+Classe = Classe.Classe()  # ['Assassino', 'Arqueiro', 'Tanque']
+Raça = Raça.Raça()  # ['Anão', 'Humano', 'Goblin']
+Jubileu = Personagem.Personagem("Jubileu", Classe.get_classe()[0], Raça.get_raca()[0])
+print(Jubileu.get_nome())
+print(Jubileu.get_classe())
+print(Jubileu.get_raca())
 
 # ItemObj = Item.Item('Item Genérico', 'Poção')
 # print(ItemObj.get_nome())
 # print(ItemObj.get_tipo())
 
-ArmaObj = Arma.Arma('Espada das Trevas', 50)
-print(ArmaObj.get_nome())
-print(ArmaObj.get_tipo())
-print(ArmaObj.get_ataque())
+# ArmaObj = Arma.Arma('Espada das Trevas', 50)
+# print(ArmaObj.get_nome())
+# print(ArmaObj.get_tipo())
+# print(ArmaObj.get_ataque())
 
 # print(Jubileu.get_raca())
 # Jubileu.set_raca('Humano')
