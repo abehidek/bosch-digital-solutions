b7e660b6522bceda2100a0d27b128f0d40102fc9
abehidek <hidek.abe@outlook.com>
2022-05-20 15:43:02 -0300

    Add click event to pyqt material

--- fundamentos-programação/python/pyqt/main.py
+++ fundamentos-programação/python/pyqt/main.py
@@ -1,8 +1,21 @@
 from PyQt5 import uic, QtWidgets
 import sys
 
-app = QtWidgets.QApplication([])
-screen = uic.loadUi("fundamentos-programação/python/pyqt/view/screen.ui")
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi("fundamentos-programação/python/pyqt/view/screen.ui", self)
 
-screen.show()
-app.exec()
+        self.button = self.findChild(QtWidgets.QPushButton, 'pushButton')
+        self.button.clicked.connect(self.func)
+        self.show()
+
+    def func(self):
+        print("Nao")
+
+app = QtWidgets.QApplication([])
+# screen = uic.loadUi()
+
+# screen.show()
+window = Ui()
+app.exec_()
