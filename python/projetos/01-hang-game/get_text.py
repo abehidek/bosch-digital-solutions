5af27c8343b51fa3b969f3537c0b7e71c5e53243
abehidek <hidek.abe@outlook.com>
2022-09-02 14:07:17 -0300

    Delete unused files

--- python/projetos/01-hang-game/get_text.py
+++
@@ -1,6 +0,0 @@
-# word = "abacaxi"
-# tip = "fruta"
-
-f = open("palavras.txt", "r").read()
-words_arr = f.replace("\n", "").split(";")
-print(words_arr[0].split(":")[0])
--- python/projetos/01-hang-game/time.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-from os import system
-
-count = 10
-while count > 0:
-    print(count)
-    sleep(1)
-    count -= 1
-    system("cls")
