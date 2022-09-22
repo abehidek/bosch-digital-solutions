132bef2f1d52fccc2efbfa7514b5af0c22f21184
abehidek <hidek.abe@outlook.com>
2022-09-22 16:59:18 -0300

    start: many-to-many relationship poc

---
+++ projetos/universidade/src/models/DevModel.java
@@ -0,0 +1,8 @@
+package models;
+
+import java.util.ArrayList;
+
+public class DevModel {
+    String nome;
+    ArrayList<RepoModel> repositorios = new ArrayList<>();
+}
---
+++ projetos/universidade/src/models/RepoModel.java
@@ -0,0 +1,8 @@
+package models;
+
+import java.util.ArrayList;
+
+public class RepoModel {
+    String nome;
+    ArrayList<DevModel> colaboradores = new ArrayList<>();
+}
